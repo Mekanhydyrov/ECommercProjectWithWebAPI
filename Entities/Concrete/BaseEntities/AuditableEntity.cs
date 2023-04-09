981794445222d7740eb8ca3a51a01751b0630512
Mekan Hydyrov <78173532+Mekanhydyrov@users.noreply.github.com>
2023-04-09 16:41:51 +0300

    - Entities Concrete Enums da Genders Oluşturuldu. - Entities Concrete Dtos de UserUpdateDto Oluşturuldu. - Entities Concrete Dtos de UserDto Oluşturuldu. - Entities Concrete Dtos de UserDetailDto Oluşturuldu. - Entities Concrete Dtos de UserAddDto Oluşturuldu. - Entities Concrete Dtos de User Oluşturuldu. - Entities Concrete dw BaseEntity Oluşturuldu. - Entities Concrete dw AuditableEntity Oluşturuldu. - Entities Abstract da IUpdatedEntity Oluşturuldu. - Entities Abstract da IEntity Oluşturuldu. - Entities Abstract da IDto Oluşturuldu. - Entities Abstract da IDisplayEntity Oluşturuldu. - Entities Abstract da ICreatedEntity Oluşturuldu.Add project files.

---
+++ Entities/Concrete/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdatedEntity
+    {
+        public int CreatedUserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int? UpdatedUserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? UpdatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
